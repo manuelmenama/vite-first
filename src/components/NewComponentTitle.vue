37f8ad40cdd37313ef1fee96ffef9ce339953dd3
manuelmenama <manuel.mena090589@gmail.com>
2022-11-14 16:56:31 +0100

    added component in app

---
+++ src/components/NewComponentTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+ name: 'NewComponentTitle'
+}
+</script>
+
+<template>
+  <h1>Primo giro di Vite</h1>
+</template>
+
+<style>
+  h1{
+    color: white;
+    text-shadow: 2px 2px 4px black;
+  }
+</style>
